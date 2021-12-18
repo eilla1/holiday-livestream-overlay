8c418bf185bc1489d1ecb48a18fca9ca10537019
Ella <72365100+eilla1@users.noreply.github.com>
2021-12-18 12:41:20 -0800

    Fix channel name

--- channel-whitelist.js
+++ channel-whitelist.js
@@ -1,5 +1,5 @@
 const channelWhitelist = id => ({
-  'C0159TSJVH8': 'what-is-my-channel-id',
+  'C0159TSJVH8': 'what-is-my-slack-id',
   'C0266FRGV': 'lounge',
   'C0P5NE354': 'bot-spam',
   'C01EZ2MNPTQ': 'katabatic-wind',
